4e334e4d80563cf846a33f8953d074195faa7489
Charde001 <140809160+Charde001@users.noreply.github.com>
2023-08-19 18:20:36 +0530

    Update Assign3.py

--- Assign3.py
+++ Assign3.py
@@ -1,42 +1,38 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import random
+import matplotlib.pyplot as plt
 
 # Function to generate a normal distribution
 def generate_normal_distribution(mean, std, n):
-  data = np.random.normal(mean, std, n)
-  return data
-
-# Function to plot the histogram of a normal distribution
-def plot_histogram(data):
-  print(data)
-  st.bar_chart(data)
-
-# Function to download the generated data as a .csv file
-def download_data(data):
-  df = pd.DataFrame(data)
-  df.to_csv('normal_distribution.csv', index=False)
+    data = np.random.normal(mean, std, n)
+    return data
 
 # Main function
 def main():
-  # Set the title of the app
-  st.title('Normal Distribution App')
+    # Set the title of the app
+    st.title('Normal Distribution App')
 
-  # Inputs
-  mean = st.slider('Mean', 0, 100, 50)
-  std = st.slider('Standard deviation', 0, 10, 2)
-  n = st.slider('Number of samples', 10, 1000, 100)
+    # Inputs
+    mean = st.slider('Mean', 0, 100, 50)
+    std = st.slider('Standard deviation', 0, 10, 2)
+    n = st.slider('Number of samples', 10, 1000, 100)
 
-  # Generate the normal distribution
-  data = generate_normal_distribution(mean, std, n)
+    # Generate the normal distribution
+    data = generate_normal_distribution(mean, std, n)
 
-  # Plot the histogram of the normal distribution
-  if st.button('Plot histogram'):
-    plot_histogram(data)
+    # Plot the histogram using Matplotlib and Streamlit
+    fig, ax = plt.subplots()
+    ax.hist(data, bins='auto', color='blue', alpha=0.7)
+    plt.xlabel("Value")
+    plt.ylabel("Frequency")
+    plt.title("Histogram of normal distribution")
+    st.pyplot(fig)
 
-  # Download the generated data as a .csv file
-  if st.button('Download data'):
-    download_data(data)
+    # Download the generated data as a .csv file
+    if st.button('Download data'):
+        df = pd.DataFrame(data)
+        df.to_csv('normal_distribution.csv', index=False)
 
-main()
+if __name__ == '__main__':
+    main()
