7d770b4fe67fabd339cf186d593fea70b24b3268
Charde001 <140809160+Charde001@users.noreply.github.com>
2023-08-19 18:12:58 +0530

    Assign3.py

---
+++ Assign3.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import random
+
+# Function to generate a normal distribution
+def generate_normal_distribution(mean, std, n):
+  data = np.random.normal(mean, std, n)
+  return data
+
+# Function to plot the histogram of a normal distribution
+def plot_histogram(data):
+  print(data)
+  st.bar_chart(data)
+
+# Function to download the generated data as a .csv file
+def download_data(data):
+  df = pd.DataFrame(data)
+  df.to_csv('normal_distribution.csv', index=False)
+
+# Main function
+def main():
+  # Set the title of the app
+  st.title('Normal Distribution App')
+
+  # Inputs
+  mean = st.slider('Mean', 0, 100, 50)
+  std = st.slider('Standard deviation', 0, 10, 2)
+  n = st.slider('Number of samples', 10, 1000, 100)
+
+  # Generate the normal distribution
+  data = generate_normal_distribution(mean, std, n)
+
+  # Plot the histogram of the normal distribution
+  if st.button('Plot histogram'):
+    plot_histogram(data)
+
+  # Download the generated data as a .csv file
+  if st.button('Download data'):
+    download_data(data)
+
+main()
